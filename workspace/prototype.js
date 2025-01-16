397db908a46a00f3277d0f339dc492a87f89d604
Banuprakash <banuprakashc@yahoo.co.in>
2025-01-16 11:01:41 +0530

    day 2

--- workspace/prototype.js
+++ workspace/prototype.js
@@ -29,18 +29,62 @@ class Person {
     }
 }
 
+// implement Serializer to clone
+
+class Serializer {
+    // Person, Address
+    constructor(types) {
+        this.types = types;
+    }
+
+    markRecursive(object) {
+        // anoint each object with a type index
+        let idx = this.types.findIndex(t => t.name === object.constructor.name);
+        if (idx !== -1) {
+            object['typeIndex'] = idx; //  Person index = 0, Address index =1
+
+            for (let key in object) {
+                if (object.hasOwnProperty(key) && object[key] != null) {
+                    this.markRecursive(object[key]);
+                }
+            }
+        }
+    }
+
+    reconstruct(object) {
+        if (object.hasOwnProperty('typeIndex')) {
+            let type = this.types[object.typeIndex];
+            let obj = new type(); // new Person(); new Address()
+            for (let key in object) {
+                if (object.hasOwnProperty(key) && object[key] != null) {
+                    obj[key] = this.reconstruct(object[key]);
+                }
+            }
+            delete obj.typeIndex;
+            return obj;
+        }
+
+        return object;
+    }
+    clone(object) {
+        // need type info; add typeIndex for  Person, Address
+        this.markRecursive(object);
+
+        let copy = JSON.parse(JSON.stringify(object)); // just raw data key/value
+        // console.log(copy);
+
+        return this.reconstruct(copy); // add typeInfo
+    }
+}
 let john = new Person('John',
     new Address('123 London Road', 'London', 'UK'));
 
-// copy only fields/state and not behaviour and type [Person , Address]
-let jane = JSON.parse(JSON.stringify(john));
+
+let serializer = new Serializer([Person, Address]);
+
+jane = serializer.clone(john);
 
 jane.name = 'Jane';
-jane.address.streetAddress = '321 Angel St';
-
-console.log(john.toString());
-console.log(jane.toString()); // inherited method
-
-john.greet();
-// this won't work, only dictionary [key/value] pairs are copied and not type info.
+console.log(jane);
 jane.greet();
+john.greet();
